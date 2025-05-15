93da4426aaa6a860ff66f3c8f4fdaad58fb97862
ayidiw <aniw985@gmail.com>
2025-05-15 14:30:24 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/login', function () {
+ //   return view('login');
+//});
+Route::get('/login',[LoginController::class, 'login']);
